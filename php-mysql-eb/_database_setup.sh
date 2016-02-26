8b7d082c3137c53e3e4cca235e9437f94a0ce9c8
Barry A Dobyns <barry@productops.com>
2016-02-26 15:52:49 -0800

    working on the issues with initial database load when there is no data present

---
+++ php-mysql-eb/_database_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DUMP=newlpef_db.sql
+
+# ONLY RESTORES IF THE DATABASE IS NOT PRESENT
+if ! echo SELECT Rid FROM T_Blocks | mysql -h $RDS_HOSTNAME -u $RDS_USERNAME --password=$RDS_PASSWORD $RDS_DB_NAME >/dev/null ; then
+    if [ -f $DUMP ] ; then
+	cat $DUMP | mysql -h $RDS_HOSTNAME -u $RDS_USERNAME --password=$RDS_PASSWORD $RDS_DB_NAME
+    else
+	echo ERROR: unable to update because $DUMP does not exist
+	exit 3
+    fi
+fi
